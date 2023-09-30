ad798492457549da8359485d9c38609d8e5260a6
LikaShl <AnzhelikaShleiko@gmail.com>
2023-09-30 18:39:31 +0200

    work with celsius and farenheit

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 function showWeather(response) {
   let currentPlace = document.querySelector("#new-city");
-  currentPlace.innerHTML = response.data.name;
   let currentTemp = document.querySelector("#current-temp");
-  let temperature = Math.round(response.data.main.temp);
-  currentTemp.innerHTML = `${temperature}`;
   let currentWeather = document.querySelector("#current-weather");
-  currentWeather.innerHTML = response.data.weather[0].description;
   let iconWeather = document.querySelector("#icon-weather");
+  let currentHumidity = document.querySelector("#current-humidity");
+  let currentWindSpeed = document.querySelector("#wind-speed");
+  let wind = Math.round(response.data.wind.speed);
+
+  celsiusTemp = response.data.main.temp;
+
+  currentPlace.innerHTML = response.data.name;
+  currentTemp.innerHTML = Math.round(celsiusTemp);
+  currentWeather.innerHTML = response.data.weather[0].description;
   iconWeather.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
   );
   iconWeather.setAttribute("alt", response.data.weather[0].description);
-  let currentHumidity = document.querySelector("#current-humidity");
   currentHumidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
-  let currentWindSpeed = document.querySelector("#wind-speed");
-  let wind = Math.round(response.data.wind.speed);
   currentWindSpeed.innerHTML = `Wind speed: ${wind} m/sec`;
 }
 
@@ -29,8 +31,6 @@ function getLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(findPosition);
 }
-let currentLocation = document.querySelector("#current-location");
-currentLocation.addEventListener("click", getLocation);
 
 function searchCity(city) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5f472b7acba333cd8a035ea85a0d4d4c&units=metric`;
@@ -42,9 +42,6 @@ function showCity(event) {
   let cityInput = document.querySelector("#search-input").value;
   searchCity(cityInput);
 }
-
-let searchingForm = document.querySelector("#search-city");
-searchingForm.addEventListener("submit", showCity);
 
 let now = new Date();
 function formatDate() {
@@ -70,7 +67,37 @@ function formatDate() {
   return today;
 }
 
+function convertToFarenheit(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#current-temp");
+  showCelsiusTemp.classList.remove("active");
+  showFarenheitTemp.classList.add("active");
+  let farenheitTemp = (celsiusTemp * 9) / 5 + 32;
+  tempElement.innerHTML = Math.round(farenheitTemp);
+}
+function convertToCelsius(event) {
+  event.preventDefault();
+  let tempElement = document.querySelector("#current-temp");
+  showFarenheitTemp.classList.remove("active");
+  showCelsiusTemp.classList.add("active");
+  tempElement.innerHTML = Math.round(celsiusTemp);
+}
+
+let currentLocation = document.querySelector("#current-location");
+currentLocation.addEventListener("click", getLocation);
+
+let searchingForm = document.querySelector("#search-city");
+searchingForm.addEventListener("submit", showCity);
+
 let showTime = document.querySelector("#curent-time");
 showTime.innerHTML = formatDate();
+
+let celsiusTemp = null;
+
+let showFarenheitTemp = document.querySelector("#farenheit-temp");
+showFarenheitTemp.addEventListener("click", convertToFarenheit);
+
+let showCelsiusTemp = document.querySelector("#celsius-temp");
+showCelsiusTemp.addEventListener("click", convertToCelsius);
 
 searchCity("Kharkiv");
